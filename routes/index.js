2bd954f6394b37ae340dba06d4ee19c5bd7830e1
shehap62 <shehapkhalil62@gmail.com>
2024-09-29 11:43:25 +0300

    edit

--- routes/index.js
+++ routes/index.js
@@ -15,14 +15,19 @@ function controllerRouting(app) {
     AppController.getStatus(req, res);
   });
 
-  router.get('/stats',(req,res)=>{
-    AppController.getStats(req,res);
-  })
+  // should return the number of users and files in DB
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
 
-  router.post('/users',(req,res)=>{
-    UsersController.postNew(req,res);
-  })
+  // User Controller
 
+  // should create a new user in DB
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  // should retrieve the user base on the token used
   router.get('/users/me', (req, res) => {
     UsersController.getMe(req, res);
   });
@@ -39,4 +44,38 @@ function controllerRouting(app) {
     AuthController.getDisconnect(req, res);
   });
 
+  // Files Controller
+
+  // should create a new file in DB and in disk
+  router.post('/files', (req, res) => {
+    FilesController.postUpload(req, res);
+  });
+
+  // should retrieve the file document based on the ID
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  // should retrieve all users file documents for a
+  // specific parentId and with pagination
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  // should set isPublic to true on the file document based on the ID
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  // should set isPublic to false on the file document based on the ID
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  // should return the content of the file document based on the ID
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 }
+
+export default controllerRouting;
